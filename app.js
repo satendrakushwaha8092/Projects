dd49b2f9188dc41a4491f9d5df892d5d73d4a539
dhanashree kol <dhanashreekolaskar@nimapinfotech.com>
2022-10-28 11:30:57 +0530

    internship_project

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const routes=require('./app/routes/user.routes')
-const bookroutes=require('./app/routes/book.routes')
-const reviewroutes=require('./app/routes/review.routes')
+
+const coolgeroutes=require('./app/routes/book.routes')
+const internroutes=require('./app/routes/review.routes')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/',routes);
-app.use('/',bookroutes);
-app.use('/',reviewroutes);
+app.use('/',coolgeroutes);
+app.use('/',internroutes);
 
 app.get('/test', (req,res) => {
     console.log(req)
